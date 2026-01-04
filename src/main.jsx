e200d0e2032fadcd1bdce1772acddaa49819c64e
sangeeth20232238-oss <sangeeth.20232238@iit.ac.lk>
2026-01-04 16:50:05 +0530

    Main.jsx fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom' // <--- CHANGED THIS
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* This automatically uses the base path from vite.config.js */}
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    {/* Switched to HashRouter for GitHub Pages compatibility */}
+    <HashRouter> 
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
